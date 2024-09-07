c54b87b20123bf1f65cee24fe9c52f75510566f9
Charles Thomas <ch@rlesthom.as>
2024-09-07 17:28:50 -0400

    fix: _src_path hook
    i keep switching between running things on a mac & linux, which changes
    the between `/home/` & `/Users/` for `_src_path` in
    `.homelab-template.yaml`
    
    update the GHA to use `/Users/` and write a post-clone hook that will
    fix `_src_path` if it's `/home`

---
+++ bin/fix_path.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+OPERATION=$1
+ROOT=$2
+REPO=$3
+ANSWERSFILE=$4
+
+cd $ROOT/$REPO
+
+yq -i '._src_path="/Users/crthomas/code/charlesthomas/homelab-template"' $ANSWERSFILE
+test -z "$(git status --porcelain)" && exit 0
+
+git add $ANSWERSFILE
+git commit -m "fix homelab-template _src_path"
